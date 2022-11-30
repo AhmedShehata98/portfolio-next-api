faff11d361a25321ed3dbe276fb4c375098c638a
ahmedshehata98 <userstore98@gmail.com>
2022-11-30 13:43:44 +0200

    separated to server && app

---
+++ pages/api/portfolio.js
@@ -0,0 +1,7 @@
+import { projectsList } from "../../ApiData/portfolioData";
+
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    res.status(200).json(projectsList);
+  }
+}
