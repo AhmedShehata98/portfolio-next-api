86ccb2bdd901d001602d674c6afeea1b202fabda
ahmedshehata98 <userstore98@gmail.com>
2022-12-01 12:08:26 +0200

    added portfolio data

--- ApiData/portfolioData.js
+++ ApiData/portfolioData.js
@@ -1,13 +1,52 @@
 export const projectsList = [
   {
     id: "1",
-    title: "Project 1",
-    mediaUrl: "/background.png",
+    title: "e-commerce dashboard",
+    mediaUrl: "/e-commerce-dashboard.png",
     discription:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae consectetur exercitationem neque!",
-    technologies: ["HTML", "CSS 3", "Javascript", "React"],
-    repositoryUrl: "https://google.com",
-    demoUrl: "https://google.com",
+      "this is a e-commerce dashboard for management your business with analytics of your Sales and user reviews",
+    technologies: [
+      "HTML 5",
+      "CSS 3",
+      "Javascript",
+      "React",
+      "redux",
+      "typescript",
+      "tailwind-css",
+    ],
+    repositoryUrl: "https://github.com/AhmedShehata98/local-js-video-player",
+    demoUrl: "https://js-video-play.netlify.app/",
+  },
+  // {
+  //   id: "3",
+  //   title: "my reads udacity",
+  //   mediaUrl: "/my-reads-udacity.png",
+  //   discription:
+  //     "this is a udacity schoolership have there shelf's with books [ currentlyReading - read - want to read ] and you can add or remove apps from any shelf and add to another shelf",
+  //   technologies: ["HTML 5", "CSS 3", "Javascript", "React"],
+  //   repositoryUrl: "https://github.com/AhmedShehata98/my-reads-udacity",
+  //   demoUrl: "#",
+  // },
+  {
+    id: "3",
+    title: "subscriber managment",
+    mediaUrl: "/subscriber-managment.png",
+    discription:
+      "this projects allows you to management your subscriber in your business",
+    technologies: ["HTML 5", "CSS 3", "Javascript"],
+    repositoryUrl: "https://github.com/AhmedShehata98/subscriber-managment",
+    demoUrl: "https://ahmedshehata98.github.io/subscriber-managment/",
+  },
+  {
+    id: "4",
+    title: "inventory flow online",
+    mediaUrl: "/my-reads-udacity.png",
+    discription:
+      "this is a inventory management system that allows you to manage your inventory and control them",
+    technologies: ["HTML 5", "CSS 3", "Javascript", "bootstrap"],
+    repositoryUrl:
+      "https://github.com/AhmedShehata98/Inventory-management-system",
+    demoUrl: "https://ahmedshehata98.github.io/Inventory-management-system/",
   },
 ];
 
