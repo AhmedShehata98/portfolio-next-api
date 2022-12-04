7d74e313ab67f482e4d8557040a3faae3fc945f7
ahmedshehata98 <userstore98@gmail.com>
2022-12-04 20:41:36 +0200

    added dump ( news ) data

---
+++ ApiData/newsFeed.js
@@ -0,0 +1,49 @@
+const newsFeed = [
+  {
+    id: "1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, magnam reiciendis. Quas deleniti animi placeat voluptas tempore ut non vero!",
+    date: "Sun Jan 04 2021",
+    media:
+      "https://colorlib.com/wp/wp-content/uploads/sites/2/creative-css3-tutorials.jpg",
+  },
+  {
+    id: "2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, magnam reiciendis. Quas deleniti animi placeat voluptas tempore ut non vero!",
+    date: "mon Dec 04 2022",
+    media:
+      "https://play-lh.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw",
+  },
+  {
+    id: "3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, magnam reiciendis. Quas deleniti animi placeat voluptas tempore ut non vero!",
+    date: "Sat Oct 11 2020",
+    media:
+      "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/fGWjtyQtG4JE7UXgaPAN",
+  },
+  {
+    id: "4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, magnam reiciendis. Quas deleniti animi placeat voluptas tempore ut non vero!",
+    date: "Tue Sep 10 2022",
+    media:
+      "https://www.sahlemahle.com/tailwind-css-jit-compiler-%D9%83%D9%84-%D9%85%D8%A7-%D8%AA%D8%AD%D8%AA%D8%A7%D8%AC-%D9%85%D8%B9%D8%B1%D9%81%D8%AA%D9%87-%D8%B9%D9%86/",
+  },
+  {
+    id: "5",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, magnam reiciendis. Quas deleniti animi placeat voluptas tempore ut non vero!",
+    date: "Mon Jun 18 2022",
+    media: "https://getbootstrap.com/docs/5.2/assets/img/bootstrap-icons.png",
+  },
+  {
+    id: "6",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, magnam reiciendis. Quas deleniti animi placeat voluptas tempore ut non vero!",
+    date: "Mon Jun 18 2022",
+    media:
+      "https://www.educative.io/cdn-cgi/image/f=auto,fit=contain,w=1200/api/page/5330288608542720/image/download/6288755792019456",
+  },
+];
